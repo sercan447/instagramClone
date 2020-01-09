91023079758736e41d91c8a1c672ef880ae4f770
sercan <sercanus@sercan-mac.local>
2020-01-09 12:51:45 -0800

    Initial Commit

---
+++ instagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by sercan on 9.01.2020.
+//  Copyright © 2020 sercan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
